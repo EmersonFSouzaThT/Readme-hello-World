77f28b6e6c6c3e8571dde311280917ceb0ed2a7a
Emerson Souza <132627716+EmersonFSouzaThT@users.noreply.github.com>
2023-10-16 23:56:29 -0300

    Create Hello-World.js

---
+++ Hello-World.js
@@ -0,0 +1,11 @@
+## Hello World
+
+O projeto Hello World, foi o primeiro contato que a maioria de todos os programadores teve com o códigos, no meu caso, foi o meu primeiro contato com o Python e com o JavaScript
+
+## Como Executa-lo?
+
+  O Hello World nada mais é que o cumprimento inicial que damos ao mundo da programação, para executa-lo você deve abrir a sua IDE de preferência e utilizar a linguagem de programação que você está aprendendo para mostrar na tela a mensagem selecionada, que no caso é Hello World!
+
+ por exemplo:  console.log('Hello World!'); Este exemplo é em JavaScript.
+
+   
